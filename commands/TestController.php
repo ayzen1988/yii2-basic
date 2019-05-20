4ae8d8cd760a4441136eeaaf022ad170cde4d592
ayzen1988 <zhangb14@mysoft.com.cn>
2019-05-20 17:51:46 +0800

    test

--- commands/TestController.php
+++ commands/TestController.php
@@ -24,10 +24,10 @@ class TestController extends Controller
         $serv = new swoole_server("127.0.0.1", 6789);
         $serv->set(array(
             'worker_num' => 1,
-            'task_worker_num' => 1,
+            'task_worker_num' => 3,
             'daemonize' => false,
-            'heartbeat_check_interval' => 60,
-            'heartbeat_idle_time' => 600,
+//            'heartbeat_check_interval' => 60,
+//            'heartbeat_idle_time' => 600,
 //            'log_file' => '/data/dt/swoole/error/' . LOG_NAME
         ));
         $serv->on('connect', array($this, 'connect'));
@@ -35,6 +35,7 @@ class TestController extends Controller
         $serv->on('task', array($this, 'task'));
         $serv->on('finish', array($this, 'finish'));
         $serv->on('close', array($this, 'close'));
+        $serv->on('workerStart', array($this, 'workerStart'));
         $serv->start();
     }
 
@@ -45,12 +46,15 @@ class TestController extends Controller
 
     public function receive($serv, $fd, $from_id, $data)
     {
-        $serv->task(array('fd' => $fd, 'data' => $data));
+        $serv->close($fd);
     }
 
     public function task($serv, $task_id, $from_id, $data)
     {
-        var_dump($data);
+        echo (date('Y-m-d H:i:s')) . ' Start Handle Task from_id=' . $from_id . "\r\n";
+        sleep(3);
+        echo 'data=' . json_encode($data) . ' from_id=' . $from_id . "\r\n";
+        echo (date('Y-m-d H:i:s')) . ' End Handle Task from_id=' . $from_id . "\r\n";
     }
 
     public function finish($serv, $task_id, $data)
@@ -61,6 +65,17 @@ class TestController extends Controller
     public function close($serv, $fd)
     {
         
+    }
+
+    public function workerStart($serv, $worker_id)
+    {
+        if ($worker_id === 0) {
+            for ($i = 0; $i < 10; ++$i) {
+                echo (date('Y-m-d H:i:s')) . ' Start Push Task i=' . $i . "\r\n";
+                $serv->task(['i' => $i, 'rand' => rand()]);
+                echo (date('Y-m-d H:i:s')) . ' End Push Task i=' . $i . "\r\n";
+            }
+        }
     }
 
 }
